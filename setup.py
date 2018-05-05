5c6ad48560875dfe13496615f421e79ac01d8e6e
Jason Hu <awaregit@gmail.com>
2018-05-04 20:40:58 -0700

    tox

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     description="Publish PCAP result to MQTT",
