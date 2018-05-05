39bfee649c35c705c493da7f4a8e32635b883bfb
Jason Hu <awaregit@gmail.com>
2018-05-04 21:48:42 -0700

    Listen ARP

--- pcapmq/cli.py
+++ pcapmq/cli.py
@@ -4,15 +4,39 @@
 import sys
 import click
 
+import pcap
+import dpkt
+
 
 @click.command()
-def main(args=None):
+@click.option('--interface', default=None, help="Name of network interface, "
+                                                "default use the first "
+                                                "interface.")
+def main(interface):
     """Console script for pcapmq."""
-    click.echo("Replace this message by putting your code into "
-               "pcapmq.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    sniffer = pcap.pcap(name=interface, promisc=True,
+                        timeout_ms=50, immediate=True)
+    sniffer.setfilter('arp')
+    decode = {
+        pcap.DLT_LOOP: dpkt.loopback.Loopback,
+        pcap.DLT_NULL: dpkt.loopback.Loopback,
+        pcap.DLT_EN10MB: dpkt.ethernet.Ethernet
+    }[sniffer.datalink()]
+    
+    click.echo('listening on %s: %s' % (sniffer.name, sniffer.filter))
+
+    try:
+        for ts, pkt in sniffer:
+            msg = '%d %r' % (ts, decode(pkt))
+            click.echo(msg)
+
+    except KeyboardInterrupt:
+        nrecv, ndrop, nifdrop = sniffer.stats()
+        click.echo('\n%d packets received by filter' % nrecv)
+        click.echo('%d packets dropped by kernel' % ndrop)
+
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())  # pylint: disable=no-value-for-parameter
